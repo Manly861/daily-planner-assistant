1bbb72389173f917e25259dc964fb4f52d7766da
Manly861 <lym861@hsd.k12.or.us>
2025-10-03 09:11:53 -0700

    Update daily_planner_assistant.py

---
+++ src/daily_planner_assistant/daily_planner_assistant.py
@@ -0,0 +1,17 @@
+"""
+Daily Planner Assistant
+by Man Ly 
+"""
+
+def main():
+    print("Welcome")
+
+if __name__ == "__name__":
+    main()
+
+amount_tasks = int(input("How many work do you have to work?: "))
+task = str(input("Type task's name here: "))
+time = float(input("Enter the time here: "))
+
+task_list = [task, time]
+print(task_list)
