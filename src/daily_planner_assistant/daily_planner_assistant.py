57dcfc794e75859949f19468ea5455ec43e57d7a
Manly861 <lym861@hsd.k12.or.us>
2025-10-03 09:13:40 -0700

    adding first set up for data

--- src/daily_planner_assistant/daily_planner_assistant.py
+++ src/daily_planner_assistant/daily_planner_assistant.py
@@ -10,7 +10,7 @@ if __name__ == "__name__":
     main()
 
 amount_tasks = int(input("How many work do you have to work?: "))
-task = str(input("Type task's name here: "))
+task = input("Type task's name here: ")
 time = float(input("Enter the time here: "))
 
 task_list = [task, time]
