b280d527a037e31d5369f7045b47b5a9fc6a8e01
Manly861 <lym861@hsd.k12.or.us>
2025-10-27 09:45:34 -0700

    adding while loops to ask user information

--- src/daily_planner_assistant/daily_planner_assistant.py
+++ src/daily_planner_assistant/daily_planner_assistant.py
@@ -9,9 +9,35 @@ def main():
 if __name__ == "__name__":
     main()
 
-amount_tasks = int(input("How many work do you have to work?: "))
-task = input("Type task's name here: ")
-time = float(input("Enter the time here: "))
 
-task_list = [task, time]
-print(task_list)
+ 
+again = "yes"
+
+# Ask user some information
+while again == "yes":
+    task = input("What is your task needed to be completed?: ")
+    time = float(
+        input("What is your estimated time for this task (in hour)?: ")
+        )
+
+    # Check the time value if it is valid and then suggest a break
+    if time <= 0 or time >= 24:
+        print("Please check your estimated time agian!")
+        print("It may be over 24 hours or a negative number" )
+        continue 
+    elif time < 1:
+        if time < 0.5: 
+            break_amount = 5
+        else:
+            break_amount = 10
+    else:
+        break_amount = 15
+    break_suggestion = "Take a " + str(break_amount) + "-minute break"
+    print(break_suggestion)
+    again = "no"
+
+# Add them into todo_list and ask user if they want to continue
+# Check if total of time values is greater than 24
+
+
+
