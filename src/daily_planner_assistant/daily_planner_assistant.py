9e90cf36b83cbd93c3b14c36ade6b1166347f9d1
Manly861 <lym861@hsd.k12.or.us>
2025-11-06 08:51:12 -0800

    adding if condition to check total of time

--- src/daily_planner_assistant/daily_planner_assistant.py
+++ src/daily_planner_assistant/daily_planner_assistant.py
@@ -9,12 +9,12 @@ def main():
 if __name__ == "__name__":
     main()
 
-
  
-again = "yes"
+todo_list = []
+again = "y"
 
 # Ask user some information
-while again == "yes":
+while again == "y":
     task = input("What is your task needed to be completed?: ")
     time = float(
         input("What is your estimated time for this task (in hour)?: ")
@@ -22,22 +22,34 @@ while again == "yes":
 
     # Check the time value if it is valid and then suggest a break
     if time <= 0 or time >= 24:
-        print("Please check your estimated time agian!")
+        print("---Please check your estimated time agian!---")
         print("It may be over 24 hours or a negative number" )
-        continue 
-    elif time < 1:
+        time = float(
+            input("What is your estimated time for this task (in hour)?: ")
+        )
+    elif time <= 1:
         if time < 0.5: 
             break_amount = 5
         else:
             break_amount = 10
     else:
         break_amount = 15
-    break_suggestion = "Take a " + str(break_amount) + "-minute break"
+    break_suggestion = "---Take a " + str(break_amount) + "-minute break---"
     print(break_suggestion)
-    again = "no"
 
-# Add them into todo_list and ask user if they want to continue
+    # Add them into todo_list and ask user if they want to continue
+    mini_list = [task, time, break_amount]
+    todo_list.append(mini_list)
+    print(todo_list)
+    again = input("Do you want to conitune?(y/n): ")
+
 # Check if total of time values is greater than 24
+total_time = sum(mini_list[1] for mini_list in todo_list)
+print(total_time)
+
+
+
+
 
 
 
