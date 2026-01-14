e633d58a8369b64defea0b9d8c1564981f89a24f
Manly861 <lym861@hsd.k12.or.us>
2026-01-14 10:15:40 -0800

    fixing code to make it readable

--- src/daily_planner_assistant/daily_planner_assistant.py
+++ src/daily_planner_assistant/daily_planner_assistant.py
@@ -8,19 +8,37 @@ def main():
 
 if __name__ == "__name__":
     main()
-
-
-todo_list = []
-again = "y"
-valid_value = True
-invalid_time = True
+    
 message = "---Please check your estimated time agian!---\n"
 message += "It should not be negative numbers, zeros, or over 24 hours"
 
+def main():
+    user_todo_list = []
+    again = "y"
+    
+    
+    # Recall get_info() function to get information
+    while again == "y":
+        mini_list = get_info()
 
-# Ask user informations about name of the tasks and estimated time for each one
-# If time value is a negative number, zero, or over 24 hours, keep asking user untill it is valid
-while again == "y":
+        # Add them into todo_list and ask user if they want to continue
+        user_todo_list.append(mini_list)
+        print(user_todo_list)
+        again = input("Do you want to conitune?(y/n): ")
+
+    # Recall build_a_schedule to provide user a suggested schedule\
+    print("\tStarting Processing...")
+    build_a_schedule(user_todo_list)
+
+    print("\tThank you for your time!")
+
+def get_info():
+    """Return 3 value that are processed from users input: name of task, estimated time, and suggested break"""
+    # Set initial value
+    invalid_time = True
+
+    # Ask user informations about task and time
+    # If time value is a negative number, zero, or over 24 hours, keep asking user untill it is valid
     task = input("What is your task needed to be completed?: ")
     while invalid_time:
         time = float(input("What is your estimated time for this task (in hour)?: "))
@@ -42,32 +60,28 @@ while again == "y":
     break_suggestion = "---Take a " + str(break_amount) + "-minute break---"
     print(break_suggestion)
 
-    # Add them into todo_list and ask user if they want to continue
-    mini_list = [task, time, break_amount]
-    todo_list.append(mini_list)
-    print(todo_list)
-    again = input("Do you want to conitune?(y/n): ")
+    return task, time, break_amount
 
-# Check if total of time values is greater than 24
-total_time = sum(mini_list[1] for mini_list in todo_list)
-print(f"Your estimated time in total is: " + str(total_time) + "hours")
-if total_time > 24:
-    print(message)
-    valid_value = False
+def build_a_schedule(todo_list):
+    """Build a schedule based on the valid values"""
+    # Set initial value
+    valid_value = True
 
-# Build a schedule based on these input
-if valid_value == True:
-    todo_list.sort(key = lambda time_value: time_value[1])
-    print(todo_list)
-    for mini_list in todo_list:
-        print("Your task is: ", mini_list[0])
-        print("Task's estimated time (in hour) is ", mini_list[1])
-        print("---Take a " + str(mini_list[2]) + " minute break---")
+    # Check if total of time values is greater than 24
+    total_time = sum(mini_list[1] for mini_list in todo_list)
+    print(f"Your estimated time in total is: " + str(total_time) + "hours")
+    if total_time > 24:
+        print(message)
+        valid_value = False
 
-print("Thank you for your time!")
-
-
-
-
+    # Build a schedule based on these input
+    if valid_value == True:
+        todo_list.sort(key = lambda time_value: time_value[1])
+        print(todo_list)
+        for mini_list in todo_list:
+            print("Your task is: ", mini_list[0])
+            print("Task's estimated time (in hour) is ", mini_list[1])
+            print("---Take a " + str(mini_list[2]) + " minute break---")
+main()
 
 
