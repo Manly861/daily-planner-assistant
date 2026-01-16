30d7c8766c3f9d46a8ec0a05efb58fd88da687d7
Manly861 <lym861@hsd.k12.or.us>
2026-01-16 09:24:53 -0800

    fixing code that task and time get any type of valu

--- src/daily_planner_assistant/daily_planner_assistant.py
+++ src/daily_planner_assistant/daily_planner_assistant.py
@@ -8,46 +8,60 @@ def main():
 
 if __name__ == "__name__":
     main()
-    
-message = "---Please check your estimated time agian!---\n"
-message += "It should not be negative numbers, zeros, or over 24 hours"
+
+# Set the invalid input of time and task message
+time_message = "---Please check your estimated time again!---\n"
+time_message += "It should not be negative numbers, zeros, or over 24 hours"
+
+task_message = "---Please check your name of task again!---\n"
+task_message += "It should not include numbers or symbols"
 
 def main():
+    """Ask user information and then provide then a schedule suggestion"""
     user_todo_list = []
     again = "y"
-    
-    
+        
     # Recall get_info() function to get information
     while again == "y":
         mini_list = get_info()
 
-        # Add them into todo_list and ask user if they want to continue
+        # Append them into todo_list and ask user if they want to continue
         user_todo_list.append(mini_list)
         print(user_todo_list)
         again = input("Do you want to conitune?(y/n): ")
 
-    # Recall build_a_schedule to provide user a suggested schedule\
+    # Recall build_a_schedule to provide user a suggested schedule
     print("\tStarting Processing...")
     build_a_schedule(user_todo_list)
-
-    print("\tThank you for your time!")
+    print("\t   ---Thank you for your time!---")
 
 def get_info():
     """Return 3 value that are processed from users input: name of task, estimated time, and suggested break"""
-    # Set initial value
+    # Set initial value to continue asking user if the time value is not valid
     invalid_time = True
+    invalid_task = True
 
     # Ask user informations about task and time
-    # If time value is a negative number, zero, or over 24 hours, keep asking user untill it is valid
-    task = input("What is your task needed to be completed?: ")
-    while invalid_time:
-        time = float(input("What is your estimated time for this task (in hour)?: "))
-        if time <= 0 or time >= 24:
-            print(message)
-            invalid_time = True
-        else:
-            invalid_time = False
+    # If name of task include numbers or symbols
+    while invalid_task:
+        task = input("What is your task needed to be completed?: ")
+        if task.isalpha():
+            invalid_task = False
+        else: 
+            print(task_message)
+            invalid_task = True
 
+    # If time value is a negative number, zero, or over 24 hours, it will ask user again
+    while invalid_time:
+        try:
+            time = float(input("What is your estimated time for this task (in hour)?: "))
+            if time <= 0 or time >= 24:
+                print(time_message)
+                invalid_time = True
+            else:
+                invalid_time = False
+        except ValueError:
+            print(f"{time_message} or a string")
     # Suggest a break based on valid time value
     if time <= 1:
         if time < 0.5: 
@@ -64,24 +78,25 @@ def get_info():
 
 def build_a_schedule(todo_list):
     """Build a schedule based on the valid values"""
-    # Set initial value
-    valid_value = True
-
-    # Check if total of time values is greater than 24
+    # If total of time values is greater than 24,
+    # it will print an invalid input message 
     total_time = sum(mini_list[1] for mini_list in todo_list)
     print(f"Your estimated time in total is: " + str(total_time) + "hours")
     if total_time > 24:
-        print(message)
+        print(time_message)
         valid_value = False
+    else:
+        valid_value = True
 
-    # Build a schedule based on these input
+    # If it doesn't, it will build a schedule
+    print("\t---Here's Your Schedule Suggestion!---")
     if valid_value == True:
         todo_list.sort(key = lambda time_value: time_value[1])
-        print(todo_list)
         for mini_list in todo_list:
             print("Your task is: ", mini_list[0])
             print("Task's estimated time (in hour) is ", mini_list[1])
             print("---Take a " + str(mini_list[2]) + " minute break---")
+
 main()
 
 
