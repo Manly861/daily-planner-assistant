403d152e48af5e6ca88eac3d9e0deb2147cb1b05
Manly861 <lym861@hsd.k12.or.us>
2026-01-27 10:03:17 -0800

    fixing code that not sort the code in chronological order from lowest to highest time.

--- src/daily_planner_assistant/daily_planner_assistant.py
+++ src/daily_planner_assistant/daily_planner_assistant.py
@@ -14,11 +14,15 @@ BLUE = '\033[34m'
 YELLOW = '\033[93m'
 RESET = '\033[0m'
 
+# Difine style of text
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
 # Set the invalid input of time and task message
 invalid_time_input_message = RED + "\n---Please check your estimated time again!---\n"
 invalid_time_input_message += "  It should not be negative numbers, zeros, or over 24 hours\n" + RESET
 invalid_task_input_message = RED + "\n---Please check your name of task again!---\n"
-invalid_task_input_message += "  It should not include numbers or symbols\n" + RESET
+invalid_task_input_message += "  It should not include numbers or symbols(expect space)\n" + RESET
 
 def main():
     """Ask user information and then provide then a schedule suggestion"""
@@ -44,10 +48,10 @@ def get_info():
     invalid_task_input = True
 
     # Ask user informations about task and time
-    # If name of task input includes numbers or symbols (exclude space),
+    # If name of task input includes numbers or symbols (expect space),
     # Then, it will print a invalid task input message
     while invalid_task_input:
-        task_input = input("What is your task needed to be completed?: ")
+        task_input = input(f"What is your {BOLD}task{RESET} needed to be completed?: ")
         if " " in task_input:
             task_input_without_space = task_input.replace(" ", "")
             if task_input_without_space.isalpha():
@@ -62,22 +66,20 @@ def get_info():
                 print(invalid_task_input_message)
                 invalid_task_input = True
 
-
     # If time input is a negative number, zero, or over 24 hours, it will ask user again and again
     while invalid_time_input:
         # Ask user if they want to enter time input in hour
         time_unit_check = input("Next, Do you want to enter time input in hour? (y/n): ")
-
         try:
             if time_unit_check == "y":
-                time_input = float(input("What is your estimated time for this task (in hour)?: "))
+                time_input = float(input(f"What is your {BOLD}estimated time{RESET} for this task {BOLD}{UNDERLINE}(in hour){RESET}?: "))
                 if time_input <= 0 or time_input >= 24:
                     print(invalid_time_input_message)
                     invalid_time_input = True
                 else:
                     invalid_time_input = False
             elif time_unit_check == "n":
-                time_input = float(input("What is your estimated time for this task (in minute)?: "))
+                time_input = float(input(f"What is your {BOLD}estimated time{RESET} for this task {BOLD}{UNDERLINE}(in minute){RESET}?: "))
                 if time_input <= 0 or time_input >= 1440:
                     print(invalid_time_input_message)
                     invalid_time_input = True
@@ -88,29 +90,21 @@ def get_info():
         except ValueError:
             print(invalid_time_input_message.replace("negative numbers, zeros, or over 24 hours", "a string or symbols"))
 
-    # Suggest a break based on valid time input
+    # Suggest a break based on valid time input (in hour)
     if invalid_task_input == False:
-        if time_unit_check == "y":
-            if time_input < 1:
-                if time_input < 0.5: 
-                    break_amount = 5
-                else:
-                    break_amount = 10
-            else:
-                break_amount = 15
-            break_suggestion = BLUE + "---Take a " + str(break_amount) + "-minute break each one hour---" + RESET
-
         if time_unit_check == "n":
-            if time_input < 60:
-                if time_input < 15:
-                    break_amount = 2
-                elif 15 <= time_input < 30: 
-                    break_amount = 5
-                else:
-                    break_amount = 10
+            time_input = round((time_input / 60), 2)
+        else: 
+            time_input = time_input
+
+        if time_input < 1:
+            if time_input < 0.5: 
+                break_amount = 5
             else:
-                break_amount = 15
-            break_suggestion = BLUE + "---Take a " + str(break_amount) + "-minute break---" + RESET
+                break_amount = 10
+        else:
+            break_amount = 15
+        break_suggestion = BLUE + "---Take a " + str(break_amount) + "-minute break---" + RESET
     print(break_suggestion)
 
     return task_input, time_input, break_amount, time_unit_check
@@ -129,14 +123,11 @@ def build_a_schedule(todo_list):
         time.sleep(0.5)
 
     # Add all the time values up in hours
-    # if it is in minutes, convert it to hours and add it up
+    # if it is in minutes, convert it to hours and add it up (possibly round to the second decimal place.)
     os.system("cls")
     total_time = 0 
     for mini_list in todo_list:
-        if mini_list[3] == "y":
-            total_time += mini_list[1]
-        if mini_list[3] == "n":
-            total_time += round(mini_list[1] / 60, 2)       
+        total_time += mini_list[1]      
 
     # If total of time values is greater than 24, it will print an invalid input message
     if total_time < 24:
@@ -157,7 +148,7 @@ def build_a_schedule(todo_list):
             if mini_list[3] == "y":
                 print(f"Task's estimated time is {mini_list[1]} hour(s)")
             else:
-                print(f"Task's estimated time is {mini_list[1]} minute(s)")
+                print(f"Task's estimated time is {round((mini_list[1] * 60),2)} minute(s)")
             print("---Take a " + str(mini_list[2]) + " minute break---\n")
 
 main()
